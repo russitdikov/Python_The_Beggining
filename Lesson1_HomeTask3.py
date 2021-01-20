4fdb8a04b873f8bc956df80d62edebf9cdd9939e
erussit <rus.sitdikov@internet.ru>
2021-01-20 12:12:13 -0800

    Lesson_1 HomeTask_1

---
+++ Lesson1_HomeTask3.py
@@ -0,0 +1,7 @@
+# 3.	Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
+# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+
+user_number = int(input('Введите число: '))
+
+print(int(f'{user_number}')+int(f'{user_number}{user_number}')+int(f'{user_number}{user_number}{user_number}'))
+
